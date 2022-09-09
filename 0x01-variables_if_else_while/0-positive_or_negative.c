62400b2cb2d9947c6440e9876e45fa231d988e20
“Zakari <obonizakari@gmail.com>
2022-09-09 15:58:24 -0700

    Alx-low

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,26 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
+ * main-Entry point
  *
  * Return: Always 0 for success
  */
-int main(void)
- {
-   int n, digit;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if (n < 0)
-    {
-      printf("%d is negative\n", n); 
-    }
-  else if (n > 0)
-    {
-      printf("%d is positive\n", n); 
-    }
-  else
-    {
-      printf("%d is zero\n", n); 
-    }
-  return (0);
+int main (void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX/2;
+	if (n<0)
+	{
+	printf("%d is negative\n", n);
+	}
+	else if (n>0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
 }
+
+
