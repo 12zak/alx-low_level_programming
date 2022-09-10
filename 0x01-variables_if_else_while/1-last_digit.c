bb062502ca85bb165514e1d179565cbf928673a2
“Zakari <obonizakari@gmail.com>
2022-09-09 17:37:53 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	digit = n % 10;
-	IF (digit < 6 && digit != 0)
+	if (digit < 6 && digit != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	}
